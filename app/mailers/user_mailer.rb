05fdceccb48924b5af7056d829abab2e71467065
10mi8o <totoma855@gmail.com>
2018-06-24 07:51:41 +0900

    user_mailer改修

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,10 @@ class UserMailer < ApplicationMailer
   def create_blog_mail(blog)
     @blog = blog
     @url = 'https://serene-waters-28718.herokuapp.com/'
-    mail(to: "totoma855@gmail.com", subject: 'blogを投稿しました')
+    mail(to: blog.user.email, subject: 'blogを投稿しました')
 
+    # 自身のアドレスでチェック　動作済み
+    # mail(to: "totoma855@gmail.com", subject: 'blogを投稿しました')
     # mail to: "自分のメールアドレス", subject: 'ブログ投稿確認のメール'
 
   end
